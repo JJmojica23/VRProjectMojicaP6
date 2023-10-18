eff37fb9d770b70bff6ba64d11b8d80079571383
JJmojica23 <jjmojica2015@gmail.com>
2023-10-18 13:44:47 -0700

    Added an Interactable and Added a Rotating Reticle
    The reticle finally works and rotates around when you aim at a teleportable carpet. I added a tennis ball that bounces around when thrown. You also now have hands.

--- VRRoomMojica/Assets/Scripts/ReticleController.cs
+++ VRRoomMojica/Assets/Scripts/ReticleController.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class ReticleController : MonoBehaviour
 {
-    private readonly float speed = 50.0f;
-    private Vector3 vector3;
     // Update is called once per frame
     void Update()
     {
-          vector3 = new Vector3(0, 1, 0);
-          transform.Rotate(speed * Time.deltaTime * vector3);   
+        transform.Rotate(0, 0.5f, 0, Space.World);  
     }
 }
